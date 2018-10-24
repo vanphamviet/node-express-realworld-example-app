888eb1c86a29ae08b3ba698818c891e75e9b0c78
Van Pham <phamvietvanc4@gmail.com>
2018-10-24 17:25:11 +0700

    update the service product

--- routes/api/products.js
+++ routes/api/products.js
@@ -31,6 +31,61 @@ router.post('/', auth.required, function(req, res, next) {
   }).catch(next);
 });
 
+router.get('/', auth.optional, function(req, res, next) {
+  console.log('Ui da');
 
+  var query = {};
+  var limit = 20;
+  var offset = 0;
+
+  if(typeof req.query.limit !== 'undefined'){
+    limit = req.query.limit;
+  }
+
+  if(typeof req.query.offset !== 'undefined'){
+    offset = req.query.offset;
+  }
+
+
+  Promise.all([
+    req.query.author ? User.findOne({username: req.query.author}) : null,
+    req.query.favorited ? User.findOne({username: req.query.favorited}) : null
+  ]).then(function(results){
+    var author = results[0];
+    var favoriter = results[1];
+
+    if(author){
+      query.author = author._id;
+    }
+
+    if(favoriter){
+      query._id = {$in: favoriter.favorites};
+    } else if(req.query.favorited){
+      query._id = {$in: []};
+    }
+
+    return Promise.all([
+      Product.find(query)
+        .limit(Number(limit))
+        .skip(Number(offset))
+        .sort({createdAt: 'desc'})
+        .populate('author')
+        .exec(),
+      Product.count(query).exec(),
+      req.payload ? User.findById(req.payload.id) : null,
+    ]).then(function(results){
+      var articles = results[0];
+      var articlesCount = results[1];
+      var user = results[2];
+
+      return res.json({
+        products: articles.map(function(article){
+          return article.toJSONFor(user);
+        }),
+        productsCount: articlesCount
+      });
+    });
+  }).catch(next);
+});
 
 module.exports = router;
